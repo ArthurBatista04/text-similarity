d164950f11c9e4d589ecc384b23f8b565eb90f85
Arthur <arthurbatista04@outlook.com>
2019-11-03 20:32:47 -0300

    public

--- exercicios/fatorial.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(define (fat x)
-    (if (= x 1) 
-        1 
-        (* x (fat(- x 1)))
-    )
-)
-
-(fat 5)
--- exercicios/numPerfeito.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(define (perfeito x y)
-    (if (= (remainder x (sub1 y)) 0) 
-        #t 
-        (perfeito(x (- y 1)))
-    )
-)
-
-(perfeito 4 4)
